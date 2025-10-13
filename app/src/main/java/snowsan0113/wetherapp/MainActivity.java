23cd6df8f5878f1bdced9339a63f42fb7a8582f1
snowsan0113 <egawayuuki0113@gmail.com>
2025-10-13 21:58:45 +0900

    【追加】ボタンクリック時に24時間表示されるように

--- app/src/main/java/snowsan0113/wetherapp/MainActivity.java
+++ app/src/main/java/snowsan0113/wetherapp/MainActivity.java
@@ -1,7 +1,14 @@
 package snowsan0113.wetherapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +18,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import snowsan0113.wetherapp.listener.ButtonListener;
 import snowsan0113.wetherapp.listener.TabClickListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private Map<String, View> one_hour_map = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,5 +48,55 @@ public class MainActivity extends AppCompatActivity {
         two_week_button.setOnClickListener(listener);
         today_tomorrow.setOnClickListener(listener);
         tab.getTabAt(0).view.setOnClickListener(new TabClickListener(this));
+
+        one_button.setTag("on");
+        one_button.setBackgroundColor(Color.parseColor("#408EE9"));
+        findViewById(R.id.onehour_grid).setVisibility(View.VISIBLE);
+
+        setupLayout();
+    }
+
+    public void setupLayout() {
+        GridLayout one_hour = findViewById(R.id.onehour_grid);
+        one_hour.removeAllViews();
+
+        for (int n = 0; n < 24; n++) {
+            LinearLayout linear = new LinearLayout(this);
+            linear.setOrientation(LinearLayout.VERTICAL);
+            linear.setGravity(Gravity.CENTER);
+
+            TextView hour_text = new TextView(this);
+            hour_text.setGravity(Gravity.CENTER);
+            hour_text.setText(String.valueOf(n));
+
+            ImageView imageView = new ImageView(this);
+            LinearLayout.LayoutParams imgParams = new LinearLayout.LayoutParams(
+                    (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 38, getResources().getDisplayMetrics()),
+                    (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 33, getResources().getDisplayMetrics())
+            );
+            imgParams.topMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources().getDisplayMetrics());
+            imageView.setLayoutParams(imgParams);
+            imageView.setImageResource(R.drawable.mark_tenki_hare);
+
+            TextView max_text = new TextView(this);
+            max_text.setText("xx℃");
+            max_text.setTextColor(Color.parseColor("#E43232"));
+            max_text.setGravity(Gravity.CENTER);
+
+            TextView min_text = new TextView(this);
+            min_text.setText("xx℃");
+            min_text.setTextColor(Color.parseColor("#3F51B5"));
+            min_text.setGravity(Gravity.CENTER);
+
+            linear.addView(hour_text);
+            linear.addView(imageView);
+            linear.addView(max_text);
+            linear.addView(min_text);
+
+            linear.setId(View.generateViewId());
+
+            one_hour.addView(linear);
+
+        }
     }
 }

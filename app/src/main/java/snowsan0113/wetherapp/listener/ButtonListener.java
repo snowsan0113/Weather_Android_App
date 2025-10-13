679a4772628913e940dac03458cb01f934e90668
snowsan0113 <egawayuuki0113@gmail.com>
2025-10-13 14:21:34 +0900

    【追加】天気のボタンで切り替えできるように追加

--- app/src/main/java/snowsan0113/wetherapp/listener/ButtonListener.java
+++ app/src/main/java/snowsan0113/wetherapp/listener/ButtonListener.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,10 +31,22 @@ public class ButtonListener implements View.OnClickListener {
             for (Button button1 : getButtonList()) {
                 button1.setTag("off");
                 button1.setBackgroundColor(Color.parseColor("#C1C3CC"));
+                for (LinearLayout layout : getLayOut()) {
+                    layout.setVisibility(View.GONE);
+                }
             }
 
             button.setTag("on");
             button.setBackgroundColor(Color.parseColor("#408EE9"));
+            if (click_view.getId() == R.id.one_hour_button) {
+                getLayOut().get(0).setVisibility(View.VISIBLE);
+            }
+            else if (click_view.getId() == R.id.today_tomorrow_button) {
+                getLayOut().get(1).setVisibility(View.VISIBLE);
+            }
+            else if (click_view.getId() == R.id.two_week_button) {
+                getLayOut().get(2).setVisibility(View.VISIBLE);
+            }
         }
     }
 
@@ -41,5 +54,11 @@ public class ButtonListener implements View.OnClickListener {
         return Arrays.asList(activity.findViewById(R.id.one_hour_button),
                 activity.findViewById(R.id.today_tomorrow_button),
                 activity.findViewById(R.id.two_week_button));
+    }
+
+    public List<LinearLayout> getLayOut() {
+        return Arrays.asList(activity.findViewById(R.id.onehour_layout),
+                activity.findViewById(R.id.today_tomorrow_layout),
+                activity.findViewById(R.id.twoweek_layout));
     }
 }
